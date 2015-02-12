ad59fef0fa34c3effce58e0a36a3884a25e57827
gifarikautsar <gifari13@gmail.com>
2015-02-12 11:27:41 +0700

    Update database dan Update statistik menjadi dibagi per taman

--- dump_database.sql
+++ dump_database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 12, 2015 at 04:40 AM
+-- Generation Time: Feb 12, 2015 at 04:45 AM
 -- Server version: 5.6.11
 -- PHP Version: 5.5.1
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `ppl_proyek_1`
 --
+CREATE DATABASE IF NOT EXISTS `ppl_proyek_1` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `ppl_proyek_1`;
 
 -- --------------------------------------------------------
 
@@ -45,6 +47,27 @@ INSERT INTO `pengaduan` (`id_pengaduan`, `status`, `tanggal_dibuat`, `judul`, `i
 (1, 'menunggu', '2015-02-11', 'Halo', 'Hola', 1),
 (2, 'menunggu', '2015-02-12', 'Banyak banci', 'Ada banyak banci disini', 2),
 (9, 'menunggu', '2015-02-12', 'Musiknya kurang indah', 'Banyak yang main suaranya sumbang', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `taman`
+--
+
+CREATE TABLE IF NOT EXISTS `taman` (
+  `id_taman` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  PRIMARY KEY (`id_taman`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `taman`
+--
+
+INSERT INTO `taman` (`id_taman`, `nama`, `alamat`) VALUES
+(1, 'Taman Jomblo', 'Bawah pasopati'),
+(2, 'Taman Musik Centrum', 'Depan SMAN 3 Bandung');
 
 --
 -- Constraints for dumped tables
--- web/statistik.php
+++ web/statistik.php
@@ -1,40 +1,50 @@
 <?php
 
 include 'sql_connect.php';
+$resultTaman = mysqli_query($con,"SELECT * FROM taman");
+$jumlahTaman = mysqli_num_rows($resultTaman);
 
-$resultPengaduan = mysqli_query($con,"SELECT * FROM pengaduan");
-$jumlahSemuaPengaduan = mysqli_num_rows($resultPengaduan);
+for ($i = 1; $i <= $jumlahTaman; $i++){
+	
+	$resultPengaduan = mysqli_query($con,"SELECT * FROM pengaduan WHERE id_taman = " .$i);
+	$jumlahSemuaPengaduan = mysqli_num_rows($resultPengaduan);
 
-$resultPengaduanMenunggu = mysqli_query($con,"SELECT * FROM pengaduan WHERE status='menunggu'");
-$jumlahPengaduanMenunggu = mysqli_num_rows($resultPengaduanMenunggu);
+	$resultPengaduanMenunggu = mysqli_query($con,"SELECT * FROM pengaduan WHERE status='menunggu' AND id_taman = " .$i);
+	$jumlahPengaduanMenunggu = mysqli_num_rows($resultPengaduanMenunggu);
 
-$resultPengaduanSedangDiproses = mysqli_query($con,"SELECT * FROM pengaduan WHERE status='sedang diproses'");
-$jumlahPengaduanSedangDiproses = mysqli_num_rows($resultPengaduanSedangDiproses);
+	$resultPengaduanSedangDiproses = mysqli_query($con,"SELECT * FROM pengaduan WHERE status='sedang diproses' AND id_taman = " .$i);
+	$jumlahPengaduanSedangDiproses = mysqli_num_rows($resultPengaduanSedangDiproses);
 
-$resultPengaduanSudahSelesai = mysqli_query($con,"SELECT * FROM pengaduan WHERE status='sudah selesai'");
-$jumlahPengaduanSudahSelesai = mysqli_num_rows($resultPengaduanSudahSelesai);
+	$resultPengaduanSudahSelesai = mysqli_query($con,"SELECT * FROM pengaduan WHERE status='sudah selesai' AND id_taman = " .$i);
+	$jumlahPengaduanSudahSelesai = mysqli_num_rows($resultPengaduanSudahSelesai);
 
-$resultPengaduanDitolak = mysqli_query($con,"SELECT * FROM pengaduan WHERE status='ditolak'");
-$jumlahPengaduanDitolak = mysqli_num_rows($resultPengaduanDitolak);
+	$resultPengaduanDitolak = mysqli_query($con,"SELECT * FROM pengaduan WHERE status='ditolak' AND id_taman = " .$i);
+	$jumlahPengaduanDitolak = mysqli_num_rows($resultPengaduanDitolak);
 
-echo'<table class="table table-hover">
-	<caption><p style="font-size:36px">Jumlah Semua Pengaduan Berdasar Status<br><br><br></p></caption>
-	<thead>
+	$namaTaman = mysqli_query($con,"SELECT nama FROM taman WHERE id_taman = " .$i);
+	while($rowTaman = mysqli_fetch_array($namaTaman)){
+		$Taman = $rowTaman['nama'];
+	}
+
+	echo'<table class="table table-hover">
+		<caption><p style="font-size:24px"> '.$Taman.' <br><br><br></p></caption>
+		<thead>
+			<tr>
+				<th>Jumlah</th>
+				<th>Menunggu</th>
+				<th>Sedang diproses</th>
+				<th>Sudah selesai</th>
+				<th>Ditolak</th>
+			</tr>
+		</thead>
+		<tbody>
 		<tr>
-			<th>Jumlah</th>
-			<th>Menunggu</th>
-			<th>Sedang diproses</th>
-			<th>Sudah selesai</th>
-			<th>Ditolak</th>
+			<td>'.$jumlahSemuaPengaduan.'</td>
+			<td>'.$jumlahPengaduanMenunggu.'</td>
+			<td>'.$jumlahPengaduanSedangDiproses.'</td>
+			<td>'.$jumlahPengaduanSudahSelesai.'</td>
+			<td>'.$jumlahPengaduanDitolak.'</td>
 		</tr>
-	</thead>
-	<tbody>
-	<tr>
-		<td>'.$jumlahSemuaPengaduan.'</td>
-		<td>'.$jumlahPengaduanMenunggu.'</td>
-		<td>'.$jumlahPengaduanSedangDiproses.'</td>
-		<td>'.$jumlahPengaduanSudahSelesai.'</td>
-		<td>'.$jumlahPengaduanDitolak.'</td>
-	</tr>
-	';
+		';
+	}
 ?>
